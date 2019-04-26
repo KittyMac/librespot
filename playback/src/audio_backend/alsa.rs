f1f862043a2331c3edafd27b5a8c8a49f7978eee
Rocco Bowling <rocco@chimerasw.com>
2019-04-26 10:38:10 -0400

    adding an explicity stop after error to try and combat:
    10:21:12 [ERROR] Could not start audio: Alsa error: PCM open failed
    10:21:17 [ERROR] Alsa error PCM open ALSA function 'snd_pcm_open' failed with error 'EBUSY: Device or resource busy'

--- playback/src/audio_backend/alsa.rs
+++ playback/src/audio_backend/alsa.rs
@@ -78,6 +78,7 @@ impl Sink for AlsaSink {
                 Ok(p) => self.0 = Some(p),
                 Err(e) => {
                     error!("Alsa error PCM open {}", e);
+					stop();
                     return Err(io::Error::new(
                         io::ErrorKind::Other,
                         "Alsa error: PCM open failed",
